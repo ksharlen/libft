d61835f362a0da06198bec4da3cb4c7c2bbc5e53
ksharlen <miily@list.ru>
2019-08-17 12:30:28 +0300

    new file strcasecmp.c

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 12:17:44 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/07/27 22:48:14 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/08/17 12:29:20 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -129,5 +129,6 @@ unsigned			ft_bitscpy(unsigned value, unsigned inval, unsigned num);
 size_t				ft_wcslen(const wchar_t *str);
 void				ft_ustrdel(unsigned char **str);
 char				*ft_skipfirst(char *str, int sym);
+int					ft_strcasecmp(const char *s1, const char *s2);
 
 #endif
