6eeb80f11a5a1f7c895ffe0a947083beab4bc33e
Kharazim Sharlene <ksharlen@mi-u3.21-school.ru>
2019-10-09 23:27:07 +0300

    new update libft

--- ft_strscat.c
+++ ft_strscat.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/04 15:38:48 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/10/08 18:02:20 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/10/08 20:12:10 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,10 @@ char	*ft_strscat(char *dst, char *src, const char sym)
 		while (*dst)
 			++dst;
 		ft_strncpy(dst, src, before_sym);
-		p_dst += (before_sym + 1);
+		if (ft_strchr(src, sym))
+			p_dst += (before_sym + 1);
+		else
+			p_dst += before_sym;
 	}
 	return (p_dst);
 }
