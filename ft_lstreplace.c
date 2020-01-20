9940c5d2e29bb6f2230bc3a805212016a72e24b8
ksharlen <miily@list.ru>
2020-01-20 22:43:39 +0300

    fix error from get_next_line

--- ft_lstreplace.c
+++ ft_lstreplace.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/15 01:28:22 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/05/15 01:28:23 by ksharlen         ###   ########.fr       */
+/*   Updated: 2020/01/20 22:43:06 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,18 @@
 
 t_list	*ft_lstreplace(t_list **rep, void *content, size_t content_size)
 {
+	void	*tmp;
+
 	if (rep)
 		if (*rep && content)
 		{
-			ft_lstfreeone((*rep)->content, (*rep)->content_size);
+			tmp = (*rep)->content;
 			(*rep)->content = malloc(content_size);
 			if (!(*rep))
 				return (NULL);
 			ft_memcpy((*rep)->content, content, content_size);
 			(*rep)->content_size = content_size;
+			ft_memdel(&tmp);
 			return (*rep);
 		}
 	return (NULL);
